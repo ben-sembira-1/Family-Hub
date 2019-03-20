d333492a2c96edaef137a01282c3fe99548fc6d5
Ben Sembira <40471413+sembiraben@users.noreply.github.com>
2019-03-20 21:56:30 +0200

    First Pull

--- LoginGUI/app/src/main/java/com/example/sembi/logingui/SplashScreenFirstLaunch.java
+++ LoginGUI/app/src/main/java/com/example/sembi/logingui/SplashScreenFirstLaunch.java
@@ -3,23 +3,28 @@ package com.example.sembi.logingui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenFirstLaunch extends AppCompatActivity {
 
     final int SPLASH_SCREEN_TIME_LENGTH = 5000;
+    FrameLayout topContainer, bottomContainer, mainContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screan_first_launch);
+        topContainer = findViewById(R.id.firstSplashTopGifContainer);
+        bottomContainer = findViewById(R.id.firstSplashBottomGifContainer);
+        mainContainer = findViewById(R.id.firstSplashMainGifContainer);
 
 
-        splashScreenToApp();
+        splashScreenToNextSplash();
     }
 
-    private void splashScreenToApp() {
+    private void splashScreenToNextSplash() {
 
         //TODO
         //RunAnimation();
@@ -28,7 +33,18 @@ public class SplashScreenFirstLaunch extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenFirstLaunch.this, LoginActivity.class);
+                topContainer.animate().alpha(1.0f).setDuration(SPLASH_SCREEN_TIME_LENGTH / 3);
+                bottomContainer.animate().alpha(1.0f).setDuration(SPLASH_SCREEN_TIME_LENGTH / 3);
+                mainContainer.animate().alpha(0.0f).setDuration(SPLASH_SCREEN_TIME_LENGTH / 3);
+            }
+        }, 2 * SPLASH_SCREEN_TIME_LENGTH / 3);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenFirstLaunch.this, SplashScreenSecondLaunch.class);
+                SplashScreenFirstLaunch.this.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
                 SplashScreenFirstLaunch.this.startActivity(intent);
             }
 
---
+++ LoginGUI/app/src/main/java/com/example/sembi/logingui/SplashScreenSecondLaunch.java
@@ -0,0 +1,60 @@
+package com.example.sembi.logingui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenSecondLaunch extends AppCompatActivity {
+
+    final int SPLASH_SCREEN_TIME_LENGTH = 2500;
+
+    TextView Text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_second_launch);
+        Text = findViewById(R.id.secondSplashHeaderTV);
+
+        splashScreenToNextSplash();
+    }
+
+    private void splashScreenToNextSplash() {
+
+        //TODO
+        //RunAnimation();
+        //TODO - animation with colors
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenSecondLaunch.this, LoginActivity.class);
+                SplashScreenSecondLaunch.this.startActivity(intent);
+            }
+        }, 2 * SPLASH_SCREEN_TIME_LENGTH / 5);
+
+        Text.animate().alpha(0.0f).setDuration(2 * SPLASH_SCREEN_TIME_LENGTH / 5).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.secondSplashHeaderTV3).animate().alpha(1.0f).setDuration(SPLASH_SCREEN_TIME_LENGTH / 5).
+                        withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                Text.animate().alpha(1.0f).setDuration(SPLASH_SCREEN_TIME_LENGTH / 5).
+                                        withEndAction(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                findViewById(R.id.secondSplashHeaderTV2).animate().alpha(1.0f).setDuration(SPLASH_SCREEN_TIME_LENGTH / 5);
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
+
+    }
+}
